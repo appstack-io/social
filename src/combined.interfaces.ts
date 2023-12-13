dba6efd912ee6b38011f504772bf620c1dde5850
orgal <or.gal@vee.com>
2023-12-13 19:08:38 +0200

    fix: init

---
+++ src/combined.interfaces.ts
@@ -0,0 +1,81 @@
+export interface UserFollowCreateOneInput {
+  followerId: string;
+  followeeId: string;
+}
+
+export interface UserFollowRemoveOneInput {
+  id: string;
+}
+
+export interface UserFindFollowersFilter {
+  followeeId: string;
+}
+
+export interface UserFindFollowersOpts {
+  limit: number;
+  offset: number;
+}
+
+export interface UserFindFollowersInput {
+  filter: UserFindFollowersFilter | undefined;
+  opts: UserFindFollowersOpts | undefined;
+}
+
+export interface UserFindFollowersResultMeta {
+  offset: number;
+}
+
+export interface UserFindFollowersResult {
+  meta: UserFindFollowersResultMeta | undefined;
+  followers: Follower[];
+}
+
+export interface Follower {
+  id: string;
+  createdAt: number;
+  updatedAt: number;
+  followeeId: string;
+  followerId: string;
+}
+
+export interface UserFindFolloweesFilter {
+  followerId: string;
+}
+
+export interface UserFindFolloweesOpts {
+  limit: number;
+  offset: number;
+}
+
+export interface UserFindFolloweesInput {
+  filter: UserFindFolloweesFilter | undefined;
+  opts: UserFindFolloweesOpts | undefined;
+}
+
+export interface UserFindFolloweesResultMeta {
+  offset: number;
+}
+
+export interface UserFindFolloweesResult {
+  meta: UserFindFolloweesResultMeta | undefined;
+  followees: Followee[];
+}
+
+export interface Followee {
+  id: string;
+  createdAt: number;
+  updatedAt: number;
+  followerId: string;
+  followeeId: string;
+}
+
+export interface UserFollowService {
+  CreateOne(request: UserFollowCreateOneInput): Promise<Follower>;
+  RemoveOne(request: UserFollowRemoveOneInput): Promise<Follower>;
+  FindFollowers(
+    request: UserFindFollowersInput,
+  ): Promise<UserFindFollowersResult>;
+  FindFollowees(
+    request: UserFindFolloweesInput,
+  ): Promise<UserFindFolloweesResult>;
+}
